e1cbcf52b583649f036f5a28430d8c774cc3737c
eder30001 <eder30001@hotmail.com>
2023-12-29 10:28:56 -0300

    novo2

--- Lions/__init__.py
+++ Lions/__init__.py
@@ -6,11 +6,11 @@ from sqlalchemy_utils import database_exists
 import os
 
 app = Flask(__name__)
-if os.getenv("DEBUG") == 0:
-    link_banco = os.getenv("DATABASE_URL")
-else:
-    link_banco = "sqlite:///banco.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = link_banco
+# if os.getenv("DEBUG") == 0:
+#     link_banco = os.getenv("DATABASE_URL")
+# else:
+#     link_banco = "sqlite:///banco.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "0c1b2eedbcb0812cf6ad75783d751099"
 app.config["UPLOAD_FOLDER"]="static/fotos_posts"
 
