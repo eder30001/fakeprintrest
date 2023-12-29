4a273ae7d0416bfbd5dc74a854dc43b42afe8602
eder30001 <eder30001@hotmail.com>
2023-12-29 09:47:36 -0300

    requirements.txt

--- Lions/__init__.py
+++ Lions/__init__.py
@@ -3,10 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from sqlalchemy_utils import database_exists
-
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///banco.db"
+if os.getenv("DEBUG") == 0:
+    link_banco = os.getenv("DATABASE_URL")
+else:
+    link_banco = "sqlite:///banco.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = link_banco
 app.config["SECRET_KEY"] = "0c1b2eedbcb0812cf6ad75783d751099"
 app.config["UPLOAD_FOLDER"]="static/fotos_posts"
 
